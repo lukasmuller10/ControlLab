820b0ae1f7784e380213dd7ba2fe40c0a64b74d9
muller <lukasmullerdeoliveira@gmail.com>
2021-01-31 19:33:59 -0300

    bug fix in the sixth exercice

--- alg6.m
+++ alg6.m
@@ -9,6 +9,9 @@ indice = find(y(:,2) > 0.98*ymax);
 ti = ts(indice(1):end);
 yti = ys(indice(1):end);
 
+tt = ts(1:indice(1))
+ytt = ys(1:indice(1))
+
 N = length(yti);
 res = 0;
 for n=1:N
@@ -16,14 +19,16 @@ for n=1:N
 end
 yinf = (1/N)*res;
 
-indices = find(ys < yinf); %pontos nos quais y < yinf
-i = indices(end);
-tr = ts(i);
-ind2 = find(ys < ys(tr));
-ti = ts(ind2); yi = ys(ind2);
+% indices = find(ys < yinf); %pontos nos quais y < yinf
+% i = indices(end);
+
+% tr = ti(1);
+% ind2 = find(ys < ys(indice(1)));
+% ti = ts(ind2); yi = ys(ind2);
+
 b = [];
-for i=1:length(yi)
-    b = [b log(yinf/(yinf - yi(i)))];
+for i=1:length(ytt)
+    b = [b log(yinf/(yinf - ytt(i)))];
 end
 a = (ti'*b)/(ti'*ti);
 tau = 1/a;
